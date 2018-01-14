5323b476782f0c0dfd24ab709d4123dd830b92a7
Mahmoud Taleb <mood93@hotmail.com>
2018-01-14 00:16:09 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose");
+
+//APP CONFIG
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+const PORT = process.env.PORT || 5000
+
+//MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+//RESTful ROUTES
+
+app.get("/", function(req, res) {
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res) {
+    Blog.find({}, function(err, blogs) {
+        err ? console.log("ERROR!") : res.render("index", {blogs: blogs});
+    });
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
